246411c8fc8bde9cf309ecaee8fa9fa84f3b0bc4
dofem <dallyoluwafemi@gmail.com>
2022-09-25 20:47:43 +0100

    First part of my project done

---
+++ NZWalks/NZWalks.API/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Model.Domain;
+using System.Data.Common;
+
+namespace NZWalks.API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext>options) : base(options)
+        {
+
+        }
+        public DbSet<Region> regions { get; set; }
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> walkDifficulties { get; set; }
+
+    }
+}
